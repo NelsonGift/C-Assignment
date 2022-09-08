5e70b317c17de0cd1e1302db484ffae7342d079a
Gojou-Satoru**/ <96657284+NelsonGift@users.noreply.github.com>
2022-09-08 09:42:52 +0300

    Add files via upload

---
+++ C#work/Number1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eneter number of odd numbers between 7 amd 15");
+            int odd = Convert.ToInt32(Console.ReadLine());
+            int i;
+            int sum=0;
+            Console.WriteLine("The odd numbers are:");
+            for (i=7; i<=15; i++)
+            {
+                if (i%2==1)
+                {
+                    Console.WriteLine(i);
+                    sum = sum + i;
+                }
+                
+            }
+            
+            Console.WriteLine("Their sum is:"+ sum);
+
+           
+           
+        }
+    }
+}
---
+++ C#work/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 6;
+            int[] negatives = new int[length];
+            Console.WriteLine("Enter negative numbers between -20 and -15");
+            for (int i = 0; i<length; i++)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num >= -20 && num <= -15)
+                {
+                    negatives[i] = num;
+                }
+                else
+                {
+                    Console.WriteLine("{0} has been skipped", num);
+                    i-=1;
+                }
+            }
+            Console.WriteLine("Negative numbers are [{0}]", string.Join(", ", negatives));
+            int[] reversedArr = new int[length];
+            for (int l = negatives.Length-1; l>=0; l--)
+            {
+                string formatter = ",";
+                if (l == 0)
+                {
+                    formatter = "";
+                }
+                Console.Write(negatives[l]+ ""+ formatter);
+            }
+        }
+    }
+}
